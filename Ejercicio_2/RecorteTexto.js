6cae85c00f0036e0e794bed5940eacae9d8c20db
jeasson suarez <jeassonsuarez@outlook.es>
2021-05-12 12:42:15 -0500

    segundo con operadores ternarios y comentario metodo slice

--- Ejercicio_2/RecorteTexto.js
+++ Ejercicio_2/RecorteTexto.js
@@ -7,20 +7,21 @@ let numCar
 let mostrar
 
 const traerTexto = ()=>{
-    mostrar=undefined
+    mostrar = undefined
     texto = document.getElementById("texto").value
     numCar = document.getElementById("numCar").value
-    if (numCar>texto.length || numCar<0) {
-
-        window.alert("El numero de caracteres indicado supera el total de caracteres de la cadena.");
-
-    } else {
-        
-        document.getElementById("textoRecortado").value = mostrarCaracteres(texto, numCar)
-    
-    }
-
+    window.alert(texto)
+    recorte(texto, numCar, mostrar)
 }
+
+const recorte = (texto, numCar, mostrar) => 
+  (numCar<0)
+    ?window.alert("El numeri de caracteres indicado es negativo")
+    :(numCar>texto.length)
+       ?window.alert("El numero de caracteres indicado supera el total de caracteres de la cadena.")
+       :(texto === "")
+          ?window.alert("No se ha ingresado ningun texto")
+          :document.getElementById("textoRecortado").value = mostrarCaracteres(texto, numCar)
 
 const mostrarCaracteres=function(texto, numCar){
     
@@ -41,3 +42,5 @@ const mostrarCaracteres=function(texto, numCar){
     return mostrar
 
 }
+
+/* se puede hacer uso del metodo slice(0, numero donde se quiere cortar la cadena)*/
