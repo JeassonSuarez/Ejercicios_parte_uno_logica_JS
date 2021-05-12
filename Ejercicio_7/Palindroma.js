b4c724d88fdef720a26121a1fb340ec9111b4e1c
jeasson suarez <jeassonsuarez@outlook.es>
2021-05-12 17:50:21 -0500

    Detetminar si una palabra es palindroma p.7

---
+++ Ejercicio_7/Palindroma.js
@@ -0,0 +1,34 @@
+let palabra
+let palInversa 
+
+const traer = () =>{
+    palInversa = undefined
+    palabra = document.getElementById("texto").value
+    validar(palabra)
+}
+
+const validar = (texto="") =>
+    (!texto)
+        ?window.alert("Debe introducir un texto")
+        :palin(palabra)
+
+const invertirPal = (palabra) =>{
+
+    for (let i = palabra.length; i>=0; i--) {
+        
+        if(palInversa===undefined){
+            palInversa = palabra.charAt(i)
+        }else{
+            palInversa += palabra.charAt(i)
+        }
+
+    }
+
+    return palInversa
+
+}
+
+const palin = (palabra) =>
+    (invertirPal(palabra) === palabra)
+        ? document.getElementById("Palindroma").value = "True"
+        : document.getElementById("Palindroma").value = "False"
