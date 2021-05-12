18148c60529ea0e3dbab2b63a155920a36b337de
jeasson suarez <jeassonsuarez@outlook.es>
2021-05-11 19:12:16 -0500

    llamado funcion de .js desde html

--- Ejercicio_1/CaracteresTexto.js
+++ Ejercicio_1/CaracteresTexto.js
@@ -3,6 +3,28 @@
 1- con arrow function y operador ternario
 */
 
+let text1 
+let numCaracteres
+
+const traerTexto = () =>{
+
+    text1 = document.getElementById("texto").value
+    return contarCaracteres(text1)
+
+}
+   
+const contarCaracteres = function(texto){
+
+    return texto.length
+
+}
+
+const devolverNumCaract = () => {
+
+      document.getElementById("numCaracteres").value=traerTexto()
+
+}
+
 const contarCaracteres1 = (texto) =>
     (!texto)
         ?console.warn("No hay ningun caracter.")
