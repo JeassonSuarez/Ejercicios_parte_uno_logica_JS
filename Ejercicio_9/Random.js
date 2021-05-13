f95c3b45cdc59db5772d91065d40aa0a9c0b1ce4
jeasson suarez <jeassonsuarez@outlook.es>
2021-05-12 20:57:15 -0500

    random entre min y max  p.9

---
+++ Ejercicio_9/Random.js
@@ -0,0 +1,17 @@
+let min 
+let max
+
+const traer = () =>{
+    min = parseInt(document.getElementById("min").value)
+    max = parseInt(document.getElementById("max").value)
+    validar(min, max)
+}
+
+const validar = (min , max) =>
+    (min >= max)
+        ?window.alert("El minimo no puede ser mayor que el maximo")
+        :(min === "")
+            ?window.alert("Digite el valor minimo")
+            :(max === "")
+                ?window.alert("Digite el valor maximo")
+                :document.getElementById("aleatorio").value =(Math.random()*(max-min))+min
