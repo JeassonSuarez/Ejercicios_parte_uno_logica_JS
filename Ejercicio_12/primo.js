563f88ae70a786be227988ae558a11a325e62a27
Jeasson S <jeassonsuarez@outlook.es>
2021-05-14 20:17:33 -0500

    Determinar si un numero es primo p.12

---
+++ Ejercicio_12/primo.js
@@ -0,0 +1,41 @@
+let numero
+
+const traer = () => {
+    document.getElementById("primo").value = ""
+    numero = document.getElementById("numero").value
+    validar(numero)
+    
+}
+
+const validar = (numero) =>
+
+    (numero === 0)
+        ?window.alert("El valor debe ser mayor a 1")
+        :(!numero)
+            ?window.alert("Ingrese un numero")    
+            :(numero <= 1 )
+                ?window.alert("El numero debe ser mayor a 1")
+                :(parseInt(numero) !== parseFloat(numero))
+                    ?window.alert("El numero debe ser un entero")
+                    :document.getElementById("primo").value = primo(parseInt(numero))
+
+const primo =(numero) =>{
+    let contador = 0
+    for (let i = numero; i >= 1; i--) {
+
+        if (numero%i === 0) {
+            
+            contador++
+
+        }
+
+    }
+
+    if (contador === 2) {
+
+        return `El numero ${numero} es primo`
+        
+    }
+
+    return `El numero ${numero} no es primo`
+}
