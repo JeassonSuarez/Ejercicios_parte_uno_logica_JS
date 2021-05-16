1f003984adbded5acfaf90045a336c2ae788d0c9
Jeasson S <jeassonsuarez@outlook.es>
2021-05-15 19:15:38 -0500

    Descuento p.17

--- Ejercicio_17/descuento.js
+++ Ejercicio_17/descuento.js
@@ -1,22 +1,26 @@
-let valor
-let descuento
+let valor = 0
+let descuento = 0 
 
 const traer = () => {
-    valor = parseFloat(document.getElementById("numero").value)
-    descuento = parseFloat(document.getElementById("descuento").value)
-    
+    valor = document.getElementById("numero").value
+    descuento = document.getElementById("descuento").value
+    console.log(document.getElementById("numero").value)
     Validar(valor, descuento)
 }
 
 const Validar = (valor, descuento) => 
-    (valor < 0)
-        ?window.alert("El valor inicial debe ser positivo")
-        :(valor < valor*(descuento/100))
-            ?window.alert("El descuento no puede ser mayor a 100%")
-            :(descuento < 0)
-                ?window.alert("El decuento debe ser un valor positivo")
-                :document.getElementById("nValor").value = calcularDes(valor, descuento)
-    
+    (!valor)
+        ?window.alert("Debe ingresar un valor inicial")
+        :(!descuento)
+            ?window.alert("Debe ingresar un descuento")
+            :(valor < 0)
+                ?window.alert("El valor inicial debe ser positivo")
+                :(valor < valor*(descuento/100))
+                    ?window.alert("El descuento no puede ser mayor a 100%")
+                    :(descuento < 0)
+                        ?window.alert("El decuento debe ser un valor positivo")
+                        :document.getElementById("nValor").value = calcularDes(valor, descuento)
+        
 const calcularDes = (valor, descuento) => {
 
     return valor-(valor*(descuento/100))
