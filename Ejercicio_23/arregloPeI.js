85d736fd8d8e7c72c631018fba2b7af4c5a46ded
Jeasson S <jeassonsuarez@outlook.es>
2021-05-19 16:59:23 -0500

    Objeto de arreglos con pares e impares de otro arreglo p.23

--- Ejercicio_23/arregloPeI.js
+++ Ejercicio_23/arregloPeI.js
@@ -2,18 +2,18 @@ let arreglo
 let esArreglo 
 
 const arreglos = {
-    arregloPares : new Array(),
-    arregloImpares : new Array()
+    arregloPares : 0,
+    arregloImpares : 0
 }
 
 const traer = () => {
     esArreglo=0
+    arreglos.arregloPares = new Array()
+    arreglos.arregloImpares = new Array()
     document.getElementById("pares").value = null
     document.getElementById("impares").value = null
     arreglo = document.getElementById("arreglo").value.split(",")
-
     validar(arreglo)
-
 }
 
 
@@ -28,7 +28,10 @@ const validar = (arreglo) => {
     }
 
     if (esArreglo === arreglo.length) {
+       
+        PeI(arreglo)
         llenarArreglosPeI(arreglo)
+
     }else{
         alert("El arreglo ingresado no cumple con el formato de numero")
     }
@@ -47,7 +50,17 @@ const llenarArreglosPeI = (arreglo) => {
 
     document.getElementById("pares").value = arreglos.arregloPares.join(",")
     document.getElementById("impares").value = arreglos.arregloImpares.join(",")
-    console.log(arreglos)
+    document.getElementById("objeto").value = `{ Arreglo de pares: [ ${arreglos.arregloPares} ] , Arreglo de impares: [ ${arreglos.arregloImpares} ] }` 
+}
+
+// aplicando el metodo de los arreglos filte, dando una condicion
+
+const PeI = (arreglo) =>{
+    const pei = {
+        pares: arreglo.filter(valor => valor%2 === 0),
+        impares: arreglo.filter(valor => valor%2 !== 0)
+    }
+    console.log(pei);
 }
 
 
