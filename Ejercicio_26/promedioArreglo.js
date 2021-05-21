13aa0394e3b83c139ebc80722c6e4f5f632658e2
Jeasson S <jeassonsuarez@outlook.es>
2021-05-20 19:22:13 -0500

    Promedio de valores en un arreglo p.25

---
+++ Ejercicio_26/promedioArreglo.js
@@ -0,0 +1,33 @@
+let arreglo
+let esArreglo
+let promedio
+
+const traer = () => {
+    esArreglo = 0
+    arreglo = document.getElementById("arreglo").value.split(",")
+    validar(arreglo)
+}
+
+const validar = (arreglo) => {
+    for (const valor of arreglo) {
+        if (isNaN(valor*valor) || /( )/g.test(valor) || valor === "") {
+            esArreglo = 0
+        }else{
+            esArreglo++
+        }
+    }
+
+    if (esArreglo === arreglo.length) {
+        document.getElementById("promedio").value = calcularPromedio(arreglo)
+    }else{
+        alert("Debe ingresar un arreglo que cumpla con el formato")
+    }
+}
+
+const calcularPromedio = (arreglo) => {
+    for (let i = 0; i < arreglo.length; i++) {
+        arreglo[i]=parseFloat(arreglo[i])
+    }
+    promedio = arreglo.reduce((a, b) => { return a + b})/arreglo.length
+    return promedio
+}
